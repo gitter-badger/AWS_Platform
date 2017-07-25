ef3a4bd2542dcaf2fab64a37380f64d4c8c50eac
chenwen666 <750912340@qq.com>
2017-07-25 21:59:54 +0800

    商户对接接口

---
+++ Hera/lib/Codes.js
@@ -0,0 +1,36 @@
+// Biz Codes defines
+export const CODES = {
+    OK : 0,
+    SystemError : 500,
+    JSONParseError : 10000,
+    DataError : 10000,
+    merchantNotExist : 10001,
+    apiKeyOrSuffixError : 10002,
+    userAlreadyRegister : 10003,
+    userNotExist : 10004,
+    passwordError : 10005,
+    Frozen : 10006,
+    merBalIns : 10007,
+    TokenError : 11000,
+}
+
+const errorMessage = {
+  "500" : "服务器错误",
+  "10000" : "数据错误",
+  "10001" : "商家不存在",
+  "10002" : "apiKey错误",
+  "10003" : "用户已注册",
+  "10004" : "用户不存在",
+  "10005" : "密码错误",
+  "10006" : "账号已被禁止",
+  "11000" : "token错误",
+  "10007" : "商家点数不足"
+}
+
+
+export class CHeraErr{
+  constructor(code){
+    this.code = code;
+    this.msg = errorMessage[code.toString()];
+  }
+}
