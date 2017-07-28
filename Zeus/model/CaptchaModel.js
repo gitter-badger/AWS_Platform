88af362a42e9b0a5a1f084624eb00655d30d8a85
cheneyweb <457299596@qq.com>
2017-07-28 10:09:59 +0800

    验证码接口增加参数处理

--- Zeus/model/CaptchaModel.js
+++ Zeus/model/CaptchaModel.js
@@ -37,14 +37,19 @@ export class CaptchaModel extends BaseModel {
      * @param {*} userLoginInfo 登录信息
      */
     async checkCaptcha(userLoginInfo) {
+        // 数据类型转换
+        userLoginInfo.captcha = parseInt(userLoginInfo.captcha)
+        // 数据校验
         if (!userLoginInfo.captcha) {
             return [BizErr.CaptchaErr(), 0]
         }
+        // 完整用户名处理
         let suffix = 'Platform'
         if (userLoginInfo.suffix) {
             suffix = userLoginInfo.suffix
         }
         const relKey = suffix + '_' + userLoginInfo.username
+        // 查询验证码
         const [err, ret] = await this.query({
             KeyConditionExpression: 'relKey = :relKey and #usage = :usage',
             FilterExpression: 'code = :code',
@@ -54,7 +59,7 @@ export class CaptchaModel extends BaseModel {
             ExpressionAttributeValues: {
                 ':relKey': relKey,
                 ':usage': 'login',
-                ':code': parseInt(userLoginInfo.captcha)
+                ':code': userLoginInfo.captcha
             }
         })
         if (err) {
