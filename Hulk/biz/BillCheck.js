c81cb82de854d92d2dfa292bf01a9968672f4a96
cheneyweb <457299596@qq.com>
2017-08-16 17:40:31 +0800

    拓展角色校验

--- Hulk/biz/BillCheck.js
+++ Hulk/biz/BillCheck.js
@@ -7,7 +7,7 @@ export class BillCheck {
     checkBill(inparam) {
         let [checkAttError, errorParams] = athena.Util.checkProperties([
             { name: "fromUserId", type: "S", min: 36, max: 36 },
-            { name: "toRole", type: "N", min: 1, max: 100 },
+            { name: "toRole", type: "N", min: 1, max: 1000 },
             { name: "toUser", type: "S", min: 6, max: 30 },
             { name: "amount", type: "REG", min: null, max: null, equal: athena.RegEnum.PRICE },
             { name: "remark", type: "NS", min: 1, max: 200 }
