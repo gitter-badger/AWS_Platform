3fc63cc762cc49c7efd22b0cca28faa263952cd4
cheneyweb <457299596@qq.com>
2017-08-24 11:21:37 +0800

    Diana接口全部trycatch

--- Diana/api_company.js
+++ Diana/api_company.js
@@ -8,119 +8,106 @@ import { CompanyCheck } from './biz/CompanyCheck'
  * 创建厂商
  */
 const companyNew = async (e, c, cb) => {
-  const errRes = { m: 'companyNew err'/*, input: e*/ }
-  const res = { m: 'companyNew' }
-  const [jsonParseErr, companyInfo] = JSONParser(e && e.body)
-  if (jsonParseErr) {
-    return ResErr(cb, jsonParseErr)
-  }
-  //检查参数是否合法
-  let [checkAttError, errorParams] = new CompanyCheck().checkCompany(companyInfo)
-  if (checkAttError) {
-    Object.assign(checkAttError, { params: errorParams })
-    return ResErr(cb, checkAttError)
-  }
-  // 获取令牌，只有管理员有权限
-  const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
-  if (tokenErr) {
-    return ResErr(cb, tokenErr)
-  }
+  try {
+    const res = { m: 'companyNew' }
+    const [jsonParseErr, companyInfo] = JSONParser(e && e.body)
+    //检查参数是否合法
+    let [checkAttError, errorParams] = new CompanyCheck().checkCompany(companyInfo)
+    // 获取令牌，只有管理员有权限
+    const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
 
-  const [addCompanyErr, addCompanyRet] = await new CompanyModel().addCompany(companyInfo)
+    // 业务操作
+    const [addCompanyErr, addCompanyRet] = await new CompanyModel().addCompany(companyInfo)
 
-  // 操作日志记录
-  companyInfo.operateAction = '创建厂商'
-  companyInfo.operateToken = token
-  new LogModel().addOperate(companyInfo, addCompanyErr, addCompanyRet)
-
-  if (addCompanyErr) {
-    return ResFail(cb, { ...errRes, err: addCompanyErr }, addCompanyErr.code)
+    // 操作日志记录
+    companyInfo.operateAction = '创建厂商'
+    companyInfo.operateToken = token
+    new LogModel().addOperate(companyInfo, addCompanyErr, addCompanyRet)
+    // 结果返回
+    if (addCompanyErr) { return ResFail(cb, { ...res, err: addCompanyErr }, addCompanyErr.code) }
+    return ResOK(cb, { ...res, payload: addCompanyRet })
+  } catch (error) {
+    return ResErr(cb, error)
   }
-  return ResOK(cb, { ...res, payload: addCompanyRet })
 }
 
 /**
  * 厂商列表
  */
 const companyList = async (e, c, cb) => {
-  const errRes = { m: 'companyList err'/*, input: e*/ }
-  const res = { m: 'companyList' }
-  const [jsonParseErr, inparam] = JSONParser(e && e.body)
-  if (jsonParseErr) {
-    return ResErr(cb, jsonParseErr)
-  }
+  try {
+    const res = { m: 'companyList' }
+    const [jsonParseErr, inparam] = JSONParser(e && e.body)
+    // 获取令牌，只有管理员有权限
+    const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
 
-  // 获取令牌，只有管理员有权限
-  const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
-  if (tokenErr) {
-    return ResErr(cb, tokenErr)
-  }
+    // 业务操作  
+    const [err, ret] = await new CompanyModel().listCompany(inparam)
 
-  const [err, ret] = await new CompanyModel().listCompany(inparam)
-  if (err) {
-    return ResFail(cb, { ...errRes, err: err }, err.code)
+    // 结果返回
+    if (err) { return ResFail(cb, { ...res, err: err }, err.code) }
+    return ResOK(cb, { ...res, payload: ret })
+  } catch (error) {
+    return ResErr(cb, error)
   }
-  return ResOK(cb, { ...res, payload: ret })
 }
 
 /**
  * 单个厂商
  */
 const companyOne = async (e, c, cb) => {
-  // 入参转换
-  const errRes = { m: 'companyOne err'/*, input: e*/ }
-  const res = { m: 'companyOne' }
-  const [paramsErr, companyParams] = Model.pathParams(e)
-  if (paramsErr) {
-    return ResErr(cb, jsonParseErr)
+  try {
+    // 入参转换
+    const res = { m: 'companyOne' }
+    const [paramsErr, companyParams] = Model.pathParams(e)
+    if (paramsErr) {
+      return ResErr(cb, jsonParseErr)
+    }
+    // 参数校验
+    if (!companyParams.companyName || !companyParams.companyId) {
+      return ResErr(cb, BizErr.InparamErr())
+    } else {
+      companyParams.companyName = decodeURIComponent(companyParams.companyName)
+    }
+    // 获取令牌，只有管理员有权限
+    const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
+
+    // 业务操作
+    let [err, ret] = await new CompanyModel().getOne(companyParams.companyName, companyParams.companyId)
+
+    // 结果返回
+    if (err) { return ResFail(cb, { ...res, err: err }, err.code) }
+    return ResOK(cb, { ...res, payload: ret })
+  } catch (error) {
+    return ResErr(cb, error)
   }
-  // 参数校验
-  if (!companyParams.companyName || !companyParams.companyId) {
-    return ResErr(cb, BizErr.InparamErr())
-  } else {
-    companyParams.companyName = decodeURIComponent(companyParams.companyName)
-  }
-  let [err, ret] = await new CompanyModel().getOne(companyParams.companyName, companyParams.companyId)
-  if (err) {
-    return ResFail(cb, { ...errRes, err: err }, err.code)
-  }
-  return ResOK(cb, { ...res, payload: ret })
 }
 
 /**
  * 厂商状态变更，接口编号：
  */
 const companyChangeStatus = async (e, c, cb) => {
-  // 数据输入，转换，校验
-  const errRes = { m: 'companyChangeStatus error' }
-  const res = { m: 'companyChangeStatus' }
-  const [jsonParseErr, inparam] = JSONParser(e && e.body)
-  if (jsonParseErr) {
-    return ResErr(cb, jsonParseErr)
-  }
-  //检查参数是否合法
-  let [checkAttError, errorParams] = new CompanyCheck().checkStatus(inparam)
-  if (checkAttError) {
-    Object.assign(checkAttError, { params: errorParams })
-    return ResErr(cb, checkAttError)
-  }
-  // 获取令牌，只有管理员有权限
-  const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
-  if (tokenErr) {
-    return ResErr(cb, tokenErr)
-  }
-  // 业务操作
-  const [err, ret] = await new CompanyModel().changeStatus(inparam.companyName, inparam.companyId, inparam.status)
+  try {
+    // 数据输入，转换，校验
+    const res = { m: 'companyChangeStatus' }
+    const [jsonParseErr, inparam] = JSONParser(e && e.body)
+    //检查参数是否合法
+    let [checkAttError, errorParams] = new CompanyCheck().checkStatus(inparam)
+    // 获取令牌，只有管理员有权限
+    const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
 
-  // 操作日志记录
-  inparam.operateAction = '厂商状态变更'
-  inparam.operateToken = token
-  new LogModel().addOperate(inparam, err, ret)
+    // 业务操作
+    const [err, ret] = await new CompanyModel().changeStatus(inparam.companyName, inparam.companyId, inparam.status)
 
-  if (err) {
-    return ResFail(cb, { ...errRes, err: err }, err.code)
-  } else {
+    // 操作日志记录
+    inparam.operateAction = '厂商状态变更'
+    inparam.operateToken = token
+    new LogModel().addOperate(inparam, err, ret)
+    // 结果返回
+    if (err) { return ResFail(cb, { ...res, err: err }, err.code) }
     return ResOK(cb, { ...res, payload: ret })
+  } catch (error) {
+    return ResErr(cb, error)
   }
 }
 
--- Diana/api_game.js
+++ Diana/api_game.js
@@ -9,131 +9,109 @@ import { GameCheck } from './biz/GameCheck'
  * 创建游戏
  */
 const gameNew = async (e, c, cb) => {
-  // 入参转换
-  const errRes = { m: 'gameNew err'/*, input: e*/ }
-  const res = { m: 'gameNew' }
-  const [jsonParseErr, gameInfo] = JSONParser(e && e.body)
-  if (jsonParseErr) {
-    return ResErr(cb, jsonParseErr)
-  }
-  //检查参数是否合法
-  let [checkAttError, errorParams] = new GameCheck().checkGame(gameInfo)
-  if (checkAttError) {
-    Object.assign(checkAttError, { params: errorParams })
-    return ResErr(cb, checkAttError)
-  }
-  // 获取令牌，只有管理员有权限
-  const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
-  if (tokenErr) {
-    return ResErr(cb, tokenErr)
-  }
+  try {
+    // 入参转换
+    const res = { m: 'gameNew' }
+    const [jsonParseErr, gameInfo] = JSONParser(e && e.body)
+    //检查参数是否合法
+    let [checkAttError, errorParams] = new GameCheck().checkGame(gameInfo)
+    // 获取令牌，只有管理员有权限
+    const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
 
-  // 业务操作
-  const [addGameInfoErr, addGameRet] = await new GameModel().addGame(gameInfo)
+    // 业务操作
+    const [addGameInfoErr, addGameRet] = await new GameModel().addGame(gameInfo)
 
-  // 操作日志记录
-  gameInfo.operateAction = '创建游戏'
-  gameInfo.operateToken = token
-  new LogModel().addOperate(gameInfo, addGameInfoErr, addGameRet)
-
-  if (addGameInfoErr) {
-    return ResFail(cb, { ...errRes, err: addGameInfoErr }, addGameInfoErr.code)
+    // 操作日志记录
+    gameInfo.operateAction = '创建游戏'
+    gameInfo.operateToken = token
+    new LogModel().addOperate(gameInfo, addGameInfoErr, addGameRet)
+    // 结果返回
+    if (addGameInfoErr) { return ResFail(cb, { ...res, err: addGameInfoErr }, addGameInfoErr.code) }
+    return ResOK(cb, { ...res, payload: addGameRet })
+  } catch (error) {
+    return ResErr(cb, error)
   }
-  return ResOK(cb, { ...res, payload: addGameRet })
 }
 
 /**
  * 游戏列表
  */
 const gameList = async (e, c, cb) => {
-  const errRes = { m: 'gamelist err'/*, input: e*/ }
-  const res = { m: 'gamelist' }
-  // const [paramsErr, gameParams] = Model.pathParams(e)
-  // if (paramsErr) {
-  //   return ResErr(cb, jsonParseErr)
-  // }
-  const [jsonParseErr, gameParams] = JSONParser(e && e.body)
-  if (jsonParseErr) {
-    return ResErr(cb, jsonParseErr)
+  try {
+    const res = { m: 'gamelist' }
+    // const [paramsErr, gameParams] = Model.pathParams(e)
+    // if (paramsErr) {
+    //   return ResErr(cb, jsonParseErr)
+    // }
+    const [jsonParseErr, gameParams] = JSONParser(e && e.body)
+    //检查参数是否合法
+    let [checkAttError, errorParams] = new GameCheck().checkQuery(gameParams)
+    let [err, ret] = [1, 1]
+    // 普通游戏列表
+    // if (!gameParams.parent || gameParams.parent == RoleCodeEnum['PlatformAdmin'] || gameParams.parent == '01') {
+    [err, ret] = await new GameModel().listGames(gameParams)
+    // }
+    // 上级用户拥有的游戏列表
+    // else {
+    // [err, ret] = await new UserModel().queryUserById(gameParams.parent)
+    // }
+    if (err) { return ResFail(cb, { ...res, err: err }, err.code) }
+    // if (gameParams.parent) {
+    //   ret = ret.gameList
+    // }
+    return ResOK(cb, { ...res, payload: ret })
+  } catch (error) {
+    return ResErr(cb, error)
   }
-
-  //检查参数是否合法
-  let [checkAttError, errorParams] = new GameCheck().checkQuery(gameParams)
-  if (checkAttError) {
-    Object.assign(checkAttError, { params: errorParams })
-    return ResErr(cb, checkAttError)
-  }
-
-  let [err, ret] = [1, 1]
-  // 普通游戏列表
-  // if (!gameParams.parent || gameParams.parent == RoleCodeEnum['PlatformAdmin'] || gameParams.parent == '01') {
-  [err, ret] = await new GameModel().listGames(gameParams)
-  // }
-  // 上级用户拥有的游戏列表
-  // else {
-  // [err, ret] = await new UserModel().queryUserById(gameParams.parent)
-  // }
-  if (err) {
-    return ResFail(cb, { ...errRes, err: err }, err.code)
-  }
-  // if (gameParams.parent) {
-  //   ret = ret.gameList
-  // }
-  return ResOK(cb, { ...res, payload: ret })
 }
 
 /**
  * 单个游戏
  */
 const gameOne = async (e, c, cb) => {
-  const errRes = { m: 'gameOne err'/*, input: e*/ }
-  const res = { m: 'gameOne' }
-  const [paramsErr, gameParams] = Model.pathParams(e)
-  if (paramsErr) {
-    return ResErr(cb, jsonParseErr)
+  try {
+    // 入参转换
+    const res = { m: 'gameOne' }
+    const [paramsErr, gameParams] = Model.pathParams(e)
+    if (paramsErr) { return ResErr(cb, jsonParseErr) }
+    // 获取令牌，只有管理员有权限
+    const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
+    // 业务操作
+    let [err, ret] = await new GameModel().getOne(gameParams.gameType, gameParams.gameId)
+    ret.gameType = GameTypeEnum[ret.gameType].name
+    // 结果返回
+    if (err) { return ResFail(cb, { ...res, err: err }, err.code) }
+    return ResOK(cb, { ...res, payload: ret })
+  } catch (error) {
+    return ResErr(cb, error)
   }
-  let [err, ret] = await new GameModel().getOne(gameParams.gameType, gameParams.gameId)
-  ret.gameType = GameTypeEnum[ret.gameType].name
-  if (err) {
-    return ResFail(cb, { ...errRes, err: err }, err.code)
-  }
-  return ResOK(cb, { ...res, payload: ret })
 }
 
 /**
  * 游戏状态变更，接口编号：
  */
 const gameChangeStatus = async (e, c, cb) => {
-  // 数据输入，转换，校验
-  const errRes = { m: 'gameChangeStatus error' }
-  const res = { m: 'gameChangeStatus' }
-  const [jsonParseErr, inparam] = JSONParser(e && e.body)
-  if (jsonParseErr) {
-    return ResErr(cb, jsonParseErr)
-  }
-  //检查参数是否合法
-  let [checkAttError, errorParams] = new GameCheck().checkStatus(inparam)
-  if (checkAttError) {
-    Object.assign(checkAttError, { params: errorParams })
-    return ResErr(cb, checkAttError)
-  }
-  // 获取令牌，只有管理员有权限
-  const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
-  if (tokenErr) {
-    return ResErr(cb, tokenErr)
-  }
-  // 业务操作
-  const [err, ret] = await new GameModel().changeStatus(inparam.gameType, inparam.gameId, inparam.status)
+  try {
+    // 数据输入，转换，校验
+    const res = { m: 'gameChangeStatus' }
+    const [jsonParseErr, inparam] = JSONParser(e && e.body)
+    //检查参数是否合法
+    let [checkAttError, errorParams] = new GameCheck().checkStatus(inparam)
+    // 获取令牌，只有管理员有权限
+    const [tokenErr, token] = await Model.currentRoleToken(e, RoleCodeEnum['PlatformAdmin'])
 
-  // 操作日志记录
-  inparam.operateAction = '游戏状态变更'
-  inparam.operateToken = token
-  new LogModel().addOperate(inparam, err, ret)
+    // 业务操作
+    const [err, ret] = await new GameModel().changeStatus(inparam.gameType, inparam.gameId, inparam.status)
 
-  if (err) {
-    return ResFail(cb, { ...errRes, err: err }, err.code)
-  } else {
+    // 操作日志记录
+    inparam.operateAction = '游戏状态变更'
+    inparam.operateToken = token
+    new LogModel().addOperate(inparam, err, ret)
+    // 结果返回
+    if (err) { return ResFail(cb, { ...res, err: err }, err.code) }
     return ResOK(cb, { ...res, payload: ret })
+  } catch (error) {
+    return ResErr(cb, error)
   }
 }
 
@@ -141,29 +119,26 @@ const gameChangeStatus = async (e, c, cb) => {
  * 游戏类别
  */
 const gameType = async (e, c, cb) => {
-  const res = { m: 'gameType' }
-  const [jsonParseErr, inparam] = JSONParser(e && e.body)
-  if (jsonParseErr) {
-    return ResErr(cb, jsonParseErr)
-  }
-  // 全部游戏类别
-  if (!inparam.parent || inparam.parent == RoleCodeEnum['PlatformAdmin'] || inparam.parent == '01') {
-    let gameTypeArr = []
-    for (let item in GameTypeEnum) {
-      gameTypeArr.push(GameTypeEnum[item])
+  try {
+    const res = { m: 'gameType' }
+    const [jsonParseErr, inparam] = JSONParser(e && e.body)
+    // 全部游戏类别
+    if (!inparam.parent || inparam.parent == RoleCodeEnum['PlatformAdmin'] || inparam.parent == '01') {
+      let gameTypeArr = []
+      for (let item in GameTypeEnum) {
+        gameTypeArr.push(GameTypeEnum[item])
+      }
+      return ResOK(cb, { ...res, payload: gameTypeArr })
     }
-    return ResOK(cb, { ...res, payload: gameTypeArr })
+    // 上级游戏类别
+    const [err, ret] = await new UserModel().queryUserById(inparam.parent)
+    return ResOK(cb, { ...res, payload: ret.gameList })
+  } catch (error) {
+    return ResErr(cb, error)
   }
-  // 上级游戏类别
-  const [err, ret] = await new UserModel().queryUserById(inparam.parent)
-  return ResOK(cb, { ...res, payload: ret.gameList })
 }
 
 // ==================== 以下为内部方法 ====================
-
-/**
-  api export
-**/
 export {
   gameNew,                      // 新建游戏
   gameList,                     // 游戏列表
