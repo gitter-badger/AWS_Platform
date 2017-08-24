01da28ed9444439b52357d9aa27e533efffa5e0b
cheneyweb <457299596@qq.com>
2017-08-24 17:38:28 +0800

    日志优化

--- Diana/model/LogModel.js
+++ Diana/model/LogModel.js
@@ -31,85 +31,6 @@ export class LogModel extends BaseModel {
     }
 
     /**
-     * 分页查询日志
-     * @param {*} inparam 
-     */
-    async logPage(inparam) {
-        // 管理员查询
-        if (!inparam.parent) {
-            return await this.page({
-                IndexName: 'LogRoleIndex',
-                Limit: inparam.pageSize,
-                ExclusiveStartKey: inparam.startKey,
-                ScanIndexForward: false,
-                KeyConditionExpression: "#role = :role",
-                FilterExpression: "#type = :type",
-                ExpressionAttributeNames: {
-                    '#role': 'role',
-                    '#type': 'type'
-                },
-                ExpressionAttributeValues: {
-                    ':role': inparam.role.toString(),
-                    ':type': inparam.type
-                }
-            }, inparam)
-        }
-        // 线路商查询 
-        else {
-            return await this.page({
-                IndexName: 'LogRoleIndex',
-                Limit: inparam.pageSize,
-                ExclusiveStartKey: inparam.startKey,
-                ScanIndexForward: false,
-                KeyConditionExpression: "#role = :role",
-                FilterExpression: "#type = :type AND #parent = :parent",
-                ExpressionAttributeNames: {
-                    '#role': 'role',
-                    '#type': 'type',
-                    '#parent': 'parent'
-                },
-                ExpressionAttributeValues: {
-                    ':role': inparam.role.toString(),
-                    ':type': inparam.type,
-                    ':parent': inparam.parent
-                }
-            }, inparam)
-        }
-        // let log = { Items: [], LastEvaluatedKey: {} }
-        // let [err, ret] = [0, 0]
-        // while (log.Items.length < inparam.pageSize && log.LastEvaluatedKey) {
-        // [err, ret] = await this.query({
-        //     IndexName: 'LogRoleIndex',
-        //     Limit: inparam.pageSize,
-        //     ExclusiveStartKey: inparam.startKey,
-        //     ScanIndexForward: false,
-        //     KeyConditionExpression: "#role = :role",
-        //     FilterExpression: "#type = :type",
-        //     ExpressionAttributeNames: {
-        //         '#role': 'role',
-        //         '#type': 'type'
-        //     },
-        //     ExpressionAttributeValues: {
-        //         ':role': inparam.role.toString(),
-        //         ':type': inparam.type
-        //     }
-        // })
-        //     if (err) {
-        //         return [err, 0]
-        //     }
-        //     // 追加数据
-        //     if (log.Items.length > 0) {
-        //         log.Items.push(...ret.Items)
-        //         log.LastEvaluatedKey = ret.LastEvaluatedKey
-        //     } else {
-        //         log = ret
-        //     }
-        //     inparam.startKey = ret.LastEvaluatedKey
-        // }
-        // return [err, ret]
-    }
-
-    /**
      * 添加操作日志
      * @param {*} inparam 
      * @param {*} error 
@@ -126,6 +47,8 @@ export class LogModel extends BaseModel {
         let inparams = inparam
         let ret = 'Y'
         let detail = result
+        let level = inparam.operateToken.level
+        let levelIndex = inparam.operateToken.levelIndex
         if (error) {
             ret = 'N'
             detail = error
@@ -135,6 +58,8 @@ export class LogModel extends BaseModel {
             userId: userId,
             role: role,
             suffix: suffix,
+            level: level,
+            levelIndex: levelIndex,
             username: username,
             lastIP: lastIP,
             type: type,
