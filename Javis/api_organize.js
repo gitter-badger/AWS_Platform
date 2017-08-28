43b462fe05c3cb81a428c52846a387210c9e16d2
cheneyweb <457299596@qq.com>
2017-08-28 14:29:15 +0800

    增加组织架构接口

---
+++ Javis/api_organize.js
@@ -0,0 +1,28 @@
+import { ResOK, ResFail, ResErr, JSONParser, BizErr, RoleCodeEnum, Model, Codes, Pick } from './lib/all'
+import { UserModel } from './model/UserModel'
+
+/**
+ * 组织架构
+ */
+const organize = async (e, c, cb) => {
+    try {
+        const res = { m: 'organize' }
+        // 入参转换和校验
+        const [jsonParseErr, inparam] = JSONParser(e && e.body)
+        // 身份令牌
+        const [tokenErr, token] = await Model.currentToken(e)
+        // 业务操作
+        const [queryErr, queryRet] = await new UserModel().organize(inparam)
+        // 结果返回
+        if (queryErr) { return ResFail(cb, { ...res, err: queryErr }, queryErr.code) }
+        return ResOK(cb, { ...res, payload: queryRet })
+    } catch (error) {
+        return ResErr(cb, error)
+    }
+}
+
+// ==================== 以下为内部方法 ====================
+
+export {
+    organize                     // 组织架构
+}
--- Javis/model/UserModel.js
+++ Javis/model/UserModel.js
@@ -12,7 +12,7 @@ import {
     RoleCodeEnum,
     RoleModels
 } from '../lib/all'
-
+import _ from 'lodash'
 import { BaseModel } from './BaseModel'
 
 export class UserModel extends BaseModel {
@@ -30,64 +30,89 @@ export class UserModel extends BaseModel {
         }
     }
     /**
-     * 根据用户ID查询
-     * @param {*} userId 用户ID 
+     * 组织架构
+     * @param {*} inparam 
      */
-    async queryUserById(userId) {
-        const [err, ret] = await this.query({
-            IndexName: 'UserIdIndex',
-            KeyConditionExpression: 'userId = :userId',
-            ExpressionAttributeValues: {
-                ':userId': userId
-            }
-        })
-        if (err) {
-            return [err, 0]
-        }
-        if (ret.Items.length - 1 != 0) {
-            return [BizErr.UserNotFoundErr(), 0]
-        }
-        return [0, ret.Items[0]]
-    }
-
-    /**
-     * 根据角色和带前缀的用户名查询唯一用户
-     * @param {*} role 
-     * @param {*} username 
-     */
-    async getUserByName(role, username) {
-        const [queryErr, queryRet] = await this.query({
-            IndexName: 'RoleUsernameIndex',
-            KeyConditionExpression: '#role = :role and #username = :username',
+    async organize(inparam) {
+        // 默认查询平台组织架构（排除平台管理员，代理）
+        let [queryErr, queryRet] = await this.scan({
+            FilterExpression: '#role <> :role AND #level <> :level',
             ExpressionAttributeNames: {
-                '#username': 'username',
-                '#role': 'role'
+                '#role': 'role',
+                '#level': 'level'
             },
             ExpressionAttributeValues: {
-                ':username': username,
-                ':role': role
+                ':role': RoleCodeEnum['Agent'],
+                ':level': 0,
             }
         })
+        // 查询代理组织架构
+        if (inparam.type == 'agent') {
+            [queryErr, queryRet] = await this.query({
+                KeyConditionExpression: '#role = :role',
+                FilterExpression: '#level <> :level',
+                ExpressionAttributeNames: {
+                    '#role': 'role',
+                    '#level': 'level'
+                },
+                ExpressionAttributeValues: {
+                    ':role': RoleCodeEnum['Agent'],
+                    ':level': 0,
+                }
+            })
+        }
         if (queryErr) {
             return [queryErr, 0]
         }
-        const User = queryRet.Items[0]
-        if (!User) {
-            return [BizErr.UserNotFoundErr(), 0]
+        // 组装组织架构的树状结构
+        let organizeTree = []
+        let childTree = []
+        for (let item of queryRet.Items) {
+            // 第一层
+            if (item.level == 1) {
+                let treeNode = { id: item.userId, parent: item.parent, name: item.displayName, children: [], size: 8192 }
+                organizeTree.push(treeNode)
+            }
+            // 剩余节点
+            else if (item.level == 2) {
+                let treeNode = { id: item.userId, parent: item.parent, name: item.displayName, children: [], size: 4096 }
+                childTree.push(treeNode)
+            } else if (item.level == 3) {
+                let treeNode = { id: item.userId, parent: item.parent, name: item.displayName, children: [], size: 2048 }
+                childTree.push(treeNode)
+            } else {
+                let treeNode = { id: item.userId, parent: item.parent, name: item.displayName, children: [], size: 1024 }
+                childTree.push(treeNode)
+            }
         }
-        return [0, User]
+        tree(organizeTree, childTree)
+        return [0, { name: 'root', children: organizeTree }]
     }
-
-    // const params = {
-    //     ...this.params,
-    //     Key: {
-    //         'role': '100',
-    //         'userId': '25f76130-e04b-4b9f-9a20-1836a75fe419'
-    //     },
-    //     UpdateExpression: "SET contractPeriod = :contractPeriod",
-    //     ExpressionAttributeValues: {
-    //         ':contractPeriod': [new Date().getTime() - 10000000, new Date().getTime() + 10000000]
-    //     }
-    // }
-    // await this.db$('update', params)
+}
+/**
+ * 组装树
+ * @param {*} treeArray 初始树（第一层）
+ * @param {*} array 剩余节点数组
+ */
+function tree(treeArray, array) {
+    // 遍历所有节点
+    for (let treeNode of treeArray) {
+        let id = treeNode.id
+        let children = treeNode.children || []
+        // 遍历剩余节点
+        for (let j = 0; j < array.length; j++) {
+            let item = array[j]
+            item.children = []
+            // 找到父亲，加入父亲节点，并从剩余节点删除
+            if (item.parent == id) {
+                children.push(item)
+                array.splice(j, 1)
+                j--
+            }
+        }
+        // 剩余节点不为0时，递归查询
+        if (array.length != 0) {
+            tree(children, array)
+        }
+    }
 }
