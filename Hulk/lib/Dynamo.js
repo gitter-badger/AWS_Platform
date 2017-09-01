8269144b03f5e8314f689a8bd612e8875538a779
cheneyweb <457299596@qq.com>
2017-09-01 11:19:56 +0800

    去掉密码生成包

--- Hulk/lib/Dynamo.js
+++ Hulk/lib/Dynamo.js
@@ -6,7 +6,6 @@ import { RoleCodeEnum } from './UserConsts'
 import _ from 'lodash'
 const bcrypt = require('bcryptjs')
 const uid = require('uuid/v4')
-const generatePassword = require('password-generator')
 AWS.config.update({ region: 'ap-southeast-1' })
 AWS.config.setPromisesDependency(require('bluebird'))
 
@@ -157,16 +156,6 @@ export const Model = {
     const result = await bcrypt.compare(pass, hash)
     return result
   },
-  genPassword: () => {
-    const minLength = 6
-    const maxLength = 16
-    let password = ''
-    let randomLength = Math.floor(Math.random() * (maxLength - minLength)) + minLength
-    while (!isStrongEnough(password)) {
-      password = generatePassword(randomLength, false, /[\w\d\?\-\.\@\#\$\%\^\&\*\(\)\+\~\!\:\{\}\;]/)
-    }
-    return password
-  },
   /**
    * IP处理
    */
@@ -244,30 +233,4 @@ export const Model = {
     }
     return false
   }
-}
-
-// 判断密码强度
-function isStrongEnough(password) {
-  const maxLength = 16
-  const minLength = 6
-  const uppercaseMinCount = 1
-  const lowercaseMinCount = 1
-  const numberMinCount = 1
-  const specialMinCount = 1
-  const UPPERCASE_RE = /([A-Z])/g
-  const LOWERCASE_RE = /([a-z])/g
-  const NUMBER_RE = /([\d])/g
-  const SPECIAL_CHAR_RE = /([\?\-])/g
-  // const NON_REPEATING_CHAR_RE = /([\w\d\?\-])\1{2,}/g
-  const uc = password.match(UPPERCASE_RE)
-  const lc = password.match(LOWERCASE_RE)
-  const n = password.match(NUMBER_RE)
-  const sc = password.match(SPECIAL_CHAR_RE)
-  // const nr = password.match(NON_REPEATING_CHAR_RE)
-  return password.length >= minLength &&
-    // !nr &&
-    uc && uc.length >= uppercaseMinCount &&
-    lc && lc.length >= lowercaseMinCount &&
-    n && n.length >= numberMinCount &&
-    sc && sc.length >= specialMinCount
 }
