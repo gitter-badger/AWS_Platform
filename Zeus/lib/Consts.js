a105a8d625553de2bf01ba3229a83c318ef82481
cheneyweb <457299596@qq.com>
2017-09-05 12:28:31 +0800

    代码精简

--- Zeus/lib/Consts.js
+++ Zeus/lib/Consts.js
@@ -7,9 +7,9 @@ export const MSNStatusEnum = {
 }
 // 游戏类型枚举
 export const GameTypeEnum = {
-  '10000': { code: '10000', name: '棋牌游戏', url: 'http://root.rottagame.com/admin/api/checkLoginAccess', desc: '', imgurl: '' },
-  '40000': { code: '40000', name: '电子游戏', url: 'http://47.88.192.69/tigergame/public/admin/api/checkLoginAccess', desc: '', imgurl: '' },
-  '30000': { code: '30000', name: '真人视讯', url: 'http://user.ybvip789.com:8080/player/login', desc: '', imgurl: '' }
+  '10000': { code: '10000', name: '棋牌游戏', url: 'http://192.168.3.131/webadmin/public/admin/api/checkLoginAccess', desc: '这是棋牌游戏', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' },
+  '40000': { code: '40000', name: '电子游戏', url: 'http://test.com', desc: '这是电子游戏', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' },
+  '30000': { code: '30000', name: '真人视讯', url: 'http://146.88.71.30:8011/player/login', desc: '这是真人视讯', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' }
 }
 /**
  * 账单实体
