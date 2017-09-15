12f02ed62f07bda784833686f48792d27e184554
cheneyweb <457299596@qq.com>
2017-09-15 14:46:29 +0800

    增加定时任务

--- nalocal/restore.js
+++ nalocal/restore.js
@@ -16,7 +16,7 @@ restore(FullBucket, 'SYSConfig_2017-09-14_12:30:06', 'SYSConfig')
  * 恢复数据表
  */
 async function restore(bucket, file, table) {
-    console.info('==========开始备份恢复==========')
+    console.info('==========开始备份恢复==========:' + moment().format('YYYY-MM-DD_HH:mm:ss'))
     try {
         let config = {
             S3Bucket: bucket,
@@ -26,7 +26,7 @@ async function restore(bucket, file, table) {
             DbRegion: DbRegion
         }
         await Restore(config)
-        console.info('==========恢复数据完成==========')
+        console.info('==========恢复数据完成==========:' + moment().format('YYYY-MM-DD_HH:mm:ss'))
     } catch (error) {
         console.error('全备份恢复捕获异常：' + error)
     }
