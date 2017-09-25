347a61a271eda5a612575de6f8fb5cbe7a8fc60b
cheneyweb <457299596@qq.com>
2017-09-25 15:15:21 +0800

    增加变更厂商接口

--- Diana/biz/CompanyCheck.js
+++ Diana/biz/CompanyCheck.js
@@ -56,4 +56,40 @@ export class CompanyCheck {
 
         return [checkAttError, errorParams]
     }
+
+    /**
+     * 检查厂商更新数据
+     * @param {*} inparam 
+     */
+    checkUpdate(inparam) {
+        let [checkAttError, errorParams] = athena.Util.checkProperties([
+            { name: "companyId", type: "S", min: 36, max: 36 },
+            { name: "companyName", type: "REG", min: null, max: null, equal: athena.RegEnum.COMPANYNAME },
+            { name: "companyContact", type: "REG", min: null, max: null, equal: athena.RegEnum.COMPANYCONTACT },
+            { name: "companyContactWay", type: "REG", min: null, max: null, equal: athena.RegEnum.COMPANYCONTACTWAY },
+            { name: "companyEmail", type: "REG", min: null, max: null, equal: athena.RegEnum.EMAIL },
+            { name: "companyRegion", type: "NS", min: 1, max: 20 },
+
+            { name: "companyDesc", type: "NS", min: 2, max: 200 },
+            { name: "companyContract", type: "NREG", min: null, max: null, equal: athena.RegEnum.URL },
+            { name: "license", type: "NREG", min: null, max: null, equal: athena.RegEnum.URL },
+            { name: "remark", type: "NS", min: 2, max: 200 }
+        ], inparam)
+
+        if (checkAttError) {
+            Object.assign(checkAttError, { params: errorParams })
+            throw checkAttError
+        }
+
+        // 数据类型处理
+        inparam.companyRegion = inparam.companyRegion ? inparam.companyRegion.toString() : Model.StringValue
+
+        inparam.companyStatus = CompanyStatusEnum.Enable
+        inparam.companyDesc = inparam.companyDesc || Model.StringValue
+        inparam.companyContract = inparam.companyContract || Model.StringValue
+        inparam.license = inparam.license || Model.StringValue
+        inparam.remark = inparam.remark || Model.StringValue
+
+        return [checkAttError, errorParams]
+    }
 }
