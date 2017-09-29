a5b2548738deaf76137ce984566c5ee181837d4b
cheneyweb <457299596@qq.com>
2017-09-29 22:05:59 +0800

    修改游戏类型

--- Zeus/lib/Consts.js
+++ Zeus/lib/Consts.js
@@ -8,23 +8,23 @@ export const MSNStatusEnum = {
 
 //测试环境
 // export const GameTypeEnum = {
-//   '10000': { code: '10000', name: '棋牌游戏', url: 'http://47.74.154.114/webadmin/public/admin/api/checkLoginAccess', desc: '这是棋牌游戏', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' },
-//   '40000': { code: '40000', name: '电子游戏', url: 'http://47.74.154.114/tigergame/public/admin/api/checkLoginAccess', desc: '这是电子游戏', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' },
-//   '30000': { code: '30000', name: '真人视讯', url: 'http://52.221.134.197:8080/gate/player/login', desc: '这是真人视讯', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' }
+//   '10000': { code: '10000', name: '棋牌游戏', url: 'http://47.74.154.114/webadmin/public/admin/api/checkLoginAccess', desc: '网络棋牌游戏正在被大家慢慢接受和喜欢我们把传统的棋牌游戏进行重新定义加上全新的呈现技术灵活的运营模式，推出了NA棋牌的产品序列', imgurl: 'http://ouef62ous.bkt.clouddn.com/2.png' },
+//   '40000': { code: '40000', name: '电子游戏', url: 'http://47.74.154.114/tigergame/public/admin/api/checkLoginAccess', desc: 'NA电子游戏融合了传统的经典电子游戏加入了更加多元化的题材以及更加具有娱乐性的玩法让更多的玩家有丰富的游戏体验', imgurl: 'http://ouef62ous.bkt.clouddn.com/4.png' },
+//   '30000': { code: '30000', name: '真人视讯', url: 'http://52.221.134.197:8080/gate/player/login', desc: '我们集合了当下最流行的真人视讯游戏类型整合了全新的互联网直播技术通过更逼真的赌场还原以及更流畅的服务，为客户提供全新的体验', imgurl: 'http://ouef62ous.bkt.clouddn.com/1.png' }
 // }
 
 // 游戏类型枚举:正式环境
 export const GameTypeEnum = {
-  '10000': { code: '10000', name: '棋牌游戏', url: 'http://tablegame.na12345.com/admin/api/checkLoginAccess', desc: '这是棋牌游戏', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' },
-  '40000': { code: '40000', name: '电子游戏', url: 'http://tigergame.na12345.com/admin/api/checkLoginAccess', desc: '这是电子游戏', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' },
-  '30000': { code: '30000', name: '真人视讯', url: 'http://52.221.134.197:8080/gate/player/login', desc: '这是真人视讯', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' }
+  '10000': { code: '10000', name: '棋牌游戏', url: 'http://tablegame.na12345.com/admin/api/checkLoginAccess', desc: '网络棋牌游戏正在被大家慢慢接受和喜欢我们把传统的棋牌游戏进行重新定义加上全新的呈现技术灵活的运营模式，推出了NA棋牌的产品序列', imgurl: 'http://ouef62ous.bkt.clouddn.com/2.png' },
+  '40000': { code: '40000', name: '电子游戏', url: 'http://tigergame.na12345.com/admin/api/checkLoginAccess', desc: 'NA电子游戏融合了传统的经典电子游戏加入了更加多元化的题材以及更加具有娱乐性的玩法让更多的玩家有丰富的游戏体验', imgurl: 'http://ouef62ous.bkt.clouddn.com/4.png' },
+  '30000': { code: '30000', name: '真人视讯', url: 'http://52.221.134.197:8080/gate/player/login', desc: '我们集合了当下最流行的真人视讯游戏类型整合了全新的互联网直播技术通过更逼真的赌场还原以及更流畅的服务，为客户提供全新的体验', imgurl: 'http://ouef62ous.bkt.clouddn.com/1.png' }
 }
 
 //游戏类型枚举:开发环境
 // export const GameTypeEnum = {
-//   '10000': { code: '10000', name: '棋牌游戏', url: 'http://47.74.154.114/webadmin/public/admin/api/checkLoginAccess', desc: '这是棋牌游戏', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' },
-//   '40000': { code: '40000', name: '电子游戏', url: 'http://47.74.154.114/tigergame/public/admin/api/checkLoginAccess', desc: '这是电子游戏', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' },
-//   '30000': { code: '30000', name: '真人视讯', url: 'http://52.221.134.197:8080/gate/player/login', desc: '这是真人视讯', imgurl: 'https://www.baidu.com/img/qixi_pad_d84d9e2020231e3c6b22aaec04296f1a.png' }
+//   '10000': { code: '10000', name: '棋牌游戏', url: 'http://47.74.154.114/webadmin/public/admin/api/checkLoginAccess', desc: '网络棋牌游戏正在被大家慢慢接受和喜欢我们把传统的棋牌游戏进行重新定义加上全新的呈现技术灵活的运营模式，推出了NA棋牌的产品序列', imgurl: 'http://ouef62ous.bkt.clouddn.com/2.png' },
+//   '40000': { code: '40000', name: '电子游戏', url: 'http://47.74.154.114/tigergame/public/admin/api/checkLoginAccess', desc: 'NA电子游戏融合了传统的经典电子游戏加入了更加多元化的题材以及更加具有娱乐性的玩法让更多的玩家有丰富的游戏体验', imgurl: 'http://ouef62ous.bkt.clouddn.com/4.png' },
+//   '30000': { code: '30000', name: '真人视讯', url: 'http://52.221.134.197:8080/gate/player/login', desc: '我们集合了当下最流行的真人视讯游戏类型整合了全新的互联网直播技术通过更逼真的赌场还原以及更流畅的服务，为客户提供全新的体验', imgurl: 'http://ouef62ous.bkt.clouddn.com/1.png' }
 // }
 
 /**
