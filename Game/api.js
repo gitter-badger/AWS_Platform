3c0de8716a8a8491e8c52753b51575e4071fff3c
cheneyweb <457299596@qq.com>
2017-09-29 16:24:05 +0800

    注释无用代码

--- Game/api.js
+++ Game/api.js
@@ -1,39 +1,39 @@
-import { ResOK, ResErr, JSONParser, BizErr, RoleCodeEnum, Model, Codes, Pick, JwtVerify, GeneratePolicyDocument } from './lib/all'
-import { TokenModel } from './model/TokenModel'
+// import { ResOK, ResErr, JSONParser, BizErr, RoleCodeEnum, Model, Codes, Pick, JwtVerify, GeneratePolicyDocument } from './lib/all'
+// import { TokenModel } from './model/TokenModel'
 // ==================== 以下为内部方法 ====================
 
 // TOKEN验证
 const jwtverify = async (e, c, cb) => {
-  // get the token from event.authorizationToken
-  const token = e.authorizationToken.split(' ')
-  if (token[0] !== 'Bearer') {
-    return c.fail('授权类型错误')
-  }
-  // verify it and return the policy statements
-  const [err, userInfo] = await JwtVerify(token[1])
-  if (err || !userInfo) {
-    console.error(JSON.stringify(err), JSON.stringify(userInfo))
-    return c.fail('未授权')
-  }
-  // 有效期校验
-  const [checkErr, checkRet] = await new TokenModel().checkExpire(userInfo)
-  if (checkErr) {
-    return c.succeed(GeneratePolicyDocument(-1, 'Allow', e.methodArn, userInfo))
-  } else {
-    // console.info('解密')
-    // console.info(Math.floor(new Date().getTime() / 1000))
-    // console.info(userInfo.iat)
-    // console.info(Math.floor((new Date().getTime() / 1000)) - userInfo.iat)
-    // if(new Date().getTime - userInfo.iat > 100000){
-    //   return c.fail('Token expire')
-    // }
-    // TOKEN是否有效校验（判断密码是否一致）
-    // if(!userInfo.password){
-    //   return c.fail('Token locked')
-    // }
-    // 结果返回
-    return c.succeed(GeneratePolicyDocument(userInfo.userId, 'Allow', e.methodArn, userInfo))
-  }
+  // // get the token from event.authorizationToken
+  // const token = e.authorizationToken.split(' ')
+  // if (token[0] !== 'Bearer') {
+  //   return c.fail('授权类型错误')
+  // }
+  // // verify it and return the policy statements
+  // const [err, userInfo] = await JwtVerify(token[1])
+  // if (err || !userInfo) {
+  //   console.error(JSON.stringify(err), JSON.stringify(userInfo))
+  //   return c.fail('未授权')
+  // }
+  // // 有效期校验
+  // const [checkErr, checkRet] = await new TokenModel().checkExpire(userInfo)
+  // if (checkErr) {
+  //   return c.succeed(GeneratePolicyDocument(-1, 'Allow', e.methodArn, userInfo))
+  // } else {
+  //   // console.info('解密')
+  //   // console.info(Math.floor(new Date().getTime() / 1000))
+  //   // console.info(userInfo.iat)
+  //   // console.info(Math.floor((new Date().getTime() / 1000)) - userInfo.iat)
+  //   // if(new Date().getTime - userInfo.iat > 100000){
+  //   //   return c.fail('Token expire')
+  //   // }
+  //   // TOKEN是否有效校验（判断密码是否一致）
+  //   // if(!userInfo.password){
+  //   //   return c.fail('Token locked')
+  //   // }
+  //   // 结果返回
+  //   return c.succeed(GeneratePolicyDocument(userInfo.userId, 'Allow', e.methodArn, userInfo))
+  // }
 }
 
 export {
