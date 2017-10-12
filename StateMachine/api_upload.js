d9831a80f5b9c9d0588d813b6a0bbb23e2b453ba
cheneyweb <457299596@qq.com>
2017-10-12 09:49:38 +0800

    增加上传图片至S3的接口

---
+++ StateMachine/api_upload.js
@@ -0,0 +1,43 @@
+import { ResOK, ResErr, JSONParser, BizErr, RoleCodeEnum, Model, Codes, Pick, S3Store$ } from './lib/all'
+const AWS = require('aws-sdk')
+const BUCKET_NAME = process.env.AWS_BUCKET_NAME
+
+/**
+ * 上传图片至S3
+ */
+const upload = async (e, c, cb) => {
+    try {
+        // 入参转换和校验
+        const [jsonParseErr, inparam] = JSONParser(e && e.body)
+        // 身份令牌
+        // const [tokenErr, token] = await Model.currentToken(e)
+        if (!inparam.contentType) {
+            return ResErr(cb, BizErr.InparamErr('Missing contentType'))
+        }
+        if (!inparam.filePath) {
+            return ResErr(cb, BizErr.InparamErr('Missing filePath'))
+        }
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: inparam.filePath,
+            Expires: 3600,
+            ContentType: inparam.contentType
+        }
+        new AWS.S3().getSignedUrl('putObject', params, (err, url) => {
+            if (err) {
+                return ResErr(cb, BizErr.InparamErr(err.message))
+            } else {
+                return ResOK(cb, { payload: url })
+            }
+        })
+    } catch (error) {
+        return ResErr(cb, error)
+    }
+}
+
+/**
+ * 对外方法
+ */
+export {
+    upload
+}
