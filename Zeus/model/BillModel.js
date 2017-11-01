b8567d599af28f2f6c8c5f84bc50ebc6b0cd365b
cheneyweb <457299596@qq.com>
2017-11-01 15:50:06 +0800

    优化管理员账单查询性能

--- Zeus/model/BillModel.js
+++ Zeus/model/BillModel.js
@@ -19,30 +19,6 @@ export class BillModel extends BaseModel {
     }
 
     /**
-     * 查询用户余额和最后一条账单记录
-     * @param {*} user 
-     */
-    async checkUserBalance(user) {
-        const [queryErr, bills] = await this.query({
-            IndexName: 'UserIdIndex',
-            KeyConditionExpression: 'userId = :userId',
-            ExpressionAttributeValues: {
-                ':userId': user.userId
-            }
-        })
-        if (queryErr) {
-            return [queryErr, 0]
-        }
-        const sums = _.reduce(bills.Items, (sum, bill) => {
-            return sum + bill.amount
-        }, 0.0)
-        let lastBill = bills.Items[bills.Items.length - 1]
-        lastBill = lastBill || {}
-        lastBill.lastBalance = user.points + sums
-        return [0, lastBill]
-    }
-
-    /**
      * 转账
      * @param {*} from 
      * @param {*} billInfo 
@@ -117,22 +93,83 @@ export class BillModel extends BaseModel {
      * @param {*} user 
      */
     async checkUserLastBill(user) {
+        // 查询最后一条账单记录
         const [queryErr, bills] = await this.query({
             IndexName: 'UserIdIndex',
+            ScanIndexForward: false,
+            Limit: 1,
             KeyConditionExpression: 'userId = :userId',
             ExpressionAttributeValues: {
                 ':userId': user.userId
             }
         })
-        if (queryErr) {
-            return [queryErr, 0]
+        if (queryErr) { return [queryErr, 0] }
+        // 内部方法查询余额
+        const [err, ret] = await this.checkUserBalance(user)
+        // 返回最后一条账单记录和余额
+        let lastBill = bills.Items[0]
+        lastBill = lastBill || {}
+        lastBill.lastBalance = ret
+        return [0, lastBill]
+    }
+
+    /**
+     * 查询用户余额
+     * @param {*} user 
+     */
+    async checkUserBalance(user) {
+        // 1、从缓存获取用户余额
+        let initPoint = user.points
+        let [cacheErr, cacheRet] = await Store$('query', {
+            TableName: Tables.SYSCacheBalance,
+            KeyConditionExpression: 'userId = :userId AND #type = :type',
+            ExpressionAttributeNames: {
+                '#type': 'type'
+            },
+            ExpressionAttributeValues: {
+                ':userId': user.userId,
+                ':type': 'ALL'
+            }
+        })
+        if (cacheErr) { return [cacheErr, 0] }
+        // 2、根据缓存是否存在进行不同处理，默认没有缓存查询所有
+        let query = {
+            IndexName: 'UserIdIndex',
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': user.userId
+            }
         }
+        // 3、缓存存在，只查询后续流水
+        if (cacheRet && !_.isEmpty(cacheRet.Items)) {
+            // 获取缓存余额
+            initPoint = cacheRet.Items[0].balance
+            let lastTime = cacheRet.Items[0].lastTime
+            // 根据最后缓存时间查询后续账单
+            query = {
+                IndexName: 'UserIdIndex',
+                KeyConditionExpression: 'userId = :userId AND createdAt > :createdAt',
+                ExpressionAttributeValues: {
+                    ':userId': user.userId,
+                    ':createdAt': lastTime
+                }
+            }
+        }
+        let [queryErr, bills] = await this.query(query)
+        if (queryErr) { return [queryErr, 0] }
+        // 4、账单汇总
         const sums = _.reduce(bills.Items, (sum, bill) => {
             return sum + bill.amount
         }, 0.0)
-        let lastBill = bills.Items[bills.Items.length - 1]
-        lastBill = lastBill || {}
-        lastBill.lastBalance = user.points + sums
-        return [0, lastBill]
+        // 5、更新用户余额缓存
+        if (!_.isEmpty(bills.Items)) {
+            [cacheErr, cacheRet] = await Store$('put', {
+                TableName: Tables.SYSCacheBalance,
+                Item: { userId: user.userId, type: 'ALL', balance: initPoint + sums, lastTime: bills.Items[bills.Items.length - 1].createdAt }
+            })
+            if (cacheErr) { return [cacheErr, 0] }
+        }
+        // 6、返回最后余额
+        return [0, initPoint + sums]
     }
 }
