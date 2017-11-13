c30286f1927028b6f158a5129c5e9d52b569d8b4
xj <799137494@qq.com>
2017-11-13 17:13:50 +0800

    增加玩家流水表触发器

--- Game/api_trigger.js
+++ Game/api_trigger.js
@@ -1,15 +1,14 @@
 import { Success, Fail, Codes, Tables, JwtVerify, JSONParser, RoleCodeEnum } from './lib/all'
 
 const billDetailTrigger = async (e, c, cb) => {
-    console.log('原始数据')
-    console.log(e.Records)
-    let record = e.Records[0].dynamodb.NewImage
-    console.info('接收到的数据')
-    console.log(record)
-    let type = record.type.N
-    let amount = record.amount.N
-    let balance = record.balance.N
-    console.log("类型：" + type + "金额：" + amount + "余额：" + balance)
+    for (let item of e.Records) {
+        let record = item.dynamodb.NewImage
+        console.info('接收到的数据')
+        let type = record.type.N
+        let amount = record.amount.N
+        let balance = record.balance.N
+        console.log("类型：" + type + "金额：" + amount + "余额：" + balance)
+    }
 }
 
 export {
