4ff0c820d19c98d60b969b069d512c18f991abbe
xj <799137494@qq.com>
2017-11-13 18:16:54 +0800

    增加玩家流水表触发器

--- Game/api_trigger.js
+++ Game/api_trigger.js
@@ -1,10 +1,11 @@
 import { Success, Fail, Codes, Tables, JwtVerify, JSONParser, RoleCodeEnum } from './lib/all'
 import { UserRankStatModel } from './model/UserRankStatModel'
+import { UserModel } from './model/UserModel'
 const billDetailTrigger = async (e, c, cb) => {
     let betCount = 0
     let winCount = 0
     let lastTime = 0
-    let userId = 0
+    let userName = ''
     console.log(e.Records[0])
     for (let item of e.Records) {
         let record = item.dynamodb.NewImage
@@ -12,8 +13,8 @@ const billDetailTrigger = async (e, c, cb) => {
         let type = record.type.N
         let amount = record.amount.N
         let balance = record.balance.N
-        let createdAt = record.createdAt
-        userId = record.userId.N
+        let createdAt = record.createdAt.N
+        userName = record.userName.S
         if (type == 3) {
             betCount += Math.abs(amount)
         } else if (type == 4) {
@@ -25,8 +26,9 @@ const billDetailTrigger = async (e, c, cb) => {
         }
         console.log('玩家id：' + userId + "类型：" + type + "金额：" + amount + "余额：" + balance)
     }
+    let [uerErr, userInfo] = await new UserModel().get({ userName }, ["userId"])
     //更新操作
-    let inparam = { userId: userId, balance: balance, betCount: betCount, winCount: winCount }
+    let inparam = { userId: userInfo.userId, balance: balance, betCount: betCount, winCount: winCount }
     new UserRankStatModel().updatRank(inparam)
 }
 
