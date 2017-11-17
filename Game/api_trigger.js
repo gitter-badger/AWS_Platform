8780055a856fd76fd3e3e61b7c6e102ae80dc903
xj <799137494@qq.com>
2017-11-17 17:43:33 +0800

    完善用户排行榜接口

--- Game/api_trigger.js
+++ Game/api_trigger.js
@@ -5,29 +5,23 @@ const billDetailTrigger = async (e, c, cb) => {
     let betCount = 0
     let winCount = 0
     let lastTime = 0
-    let balance = 0
     let userName = 'NULL!'
     for (let item of e.Records) {
         let record = item.dynamodb.NewImage
         let type = parseInt(record.type.N)
         let amount = parseFloat(record.amount.N)
-        let createdAt = parseFloat(record.createdAt.N)
         userName = record.userName.S
         if (type == 3) {
             betCount += Math.abs(parseFloat(amount))
         } else if (type == 4) {
             winCount += parseFloat(amount)
         }
-        if (lastTime < createdAt) {
-            lastTime = createdAt
-            balance = parseFloat(record.balance.N)
-        }
     }
     // 根据用户名获取UserId
-    let [uerErr, userInfo] = await new UserModel().get({ userName }, ["userId", "nickname", "headPic"])
+    let [uerErr, userInfo] = await new UserModel().get({ userName }, ["userId", "nickname", "headPic","balance"])
     //玩家没有登录不进行用户排行榜操作
     if (userInfo.nickname && userInfo.nickname != "NULL!") {
-        let inparam = { userName: userName, nickname: userInfo.nickname, headPic: userInfo.headPic, userId: parseInt(userInfo.userId), balance: balance, betCount: betCount, winCount: winCount }
+        let inparam = { userName: userName, nickname: userInfo.nickname, headPic: userInfo.headPic, userId: parseInt(userInfo.userId), balance: parseFloat(userInfo.balance), betCount: betCount, winCount: winCount }
         console.log(inparam)
         new UserRankStatModel().updateRank(inparam)
     }
