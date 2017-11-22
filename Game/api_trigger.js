a2523bc450bb5f5c8908885593f77d5c74244022
xj <799137494@qq.com>
2017-11-22 11:01:21 +0800

    测试 排行榜3

--- Game/api_trigger.js
+++ Game/api_trigger.js
@@ -6,14 +6,15 @@ const billDetailTrigger = async (e, c, cb) => {
     let winCount = 0
     let lastTime = 0
     let userName = 'NULL!'
-    console.log('打印json对象')
+    console.log('本次触发开始')
     console.log(JSON.stringify(e.Records))
+    console.log('本次触发结束')
     for (let item of e.Records) {
-
         let record = item.dynamodb.NewImage
         let type = parseInt(record.type.N)
         let amount = parseFloat(record.amount.N)
-        console.log('record:的内容有'+JSON.stringify(record))
+        console.log('record:的内容有' + JSON.stringify(record))
+        console.log('所有的用户名：' + JSON.stringify(record.userName))
         if (type == 3) {
             userName = record.userName.S
             betCount += Math.abs(parseFloat(amount))
@@ -27,7 +28,7 @@ const billDetailTrigger = async (e, c, cb) => {
     //玩家没有登录不进行用户排行榜操作
     if (userInfo.nickname && userInfo.nickname != "NULL!") {
         let inparam = { userName: userName, nickname: userInfo.nickname, headPic: userInfo.headPic, userId: parseInt(userInfo.userId), betCount: betCount, winCount: winCount }
-       console.log(inparam)
+        console.log(inparam)
         new UserRankStatModel().insertRank(inparam)
     }
 }
