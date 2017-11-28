6fdb154b3bdd2abf3c31ef5c5eb8a6593265147e
cheneyweb <457299596@qq.com>
2017-11-28 16:08:33 +0800

    准备增加用户账单流水汇总

---
+++ Stat/api_calc.js
@@ -0,0 +1,74 @@
+import { ResOK, ResErr, Codes, JSONParser, Model, RoleCodeEnum, Trim, Pick, BizErr } from './lib/all'
+import { PlayerBillModel } from './model/PlayerBillModel'
+import { SysBillModel } from './model/SysBillModel'
+/**
+ * 计算玩家流水
+ */
+const calcPlayerBill = async (e, c, cb) => {
+    try {
+        // 入参转换
+        const [jsonParseErr, inparam] = JSONParser(e && e.body)
+        // 检查参数是否合法
+        // new UserRankCheck().check(inparam)
+        // 身份令牌
+        const [tokenErr, token] = await Model.currentToken(e)
+        // 业务操作
+        const [err, ret] = await new PlayerBillModel().calcPlayerBill(inparam)
+        // 返回结果
+        if (err) { return ResErr(cb, err) }
+        return ResOK(cb, { payload: ret })
+    } catch (error) {
+        console.error(error)
+        return ResErr(cb, error)
+    }
+}
+/**
+ * 计算用户流水
+ */
+const calcUserBill = async (e, c, cb) => {
+    try {
+        // 入参转换
+        const [jsonParseErr, inparam] = JSONParser(e && e.body)
+        //检查参数是否合法
+        // const [checkAttError, errorParams] = new QueryPlayerStatCheck().check(inparam)
+        // 身份令牌
+        const [tokenErr, token] = await Model.currentToken(e)
+        // 业务操作
+        switch (inparam.role) {
+            case '1':
+                const [err1, ret1] = await new SysBillModel().calcAdminStat(inparam)
+                if (err1) { return ResErr(cb, err1) }
+                return ResOK(cb, { payload: ret1 })
+                break
+            case '10':
+                const [err10, ret10] = await new SysBillModel().calcManagerStat(inparam)
+                if (err10) { return ResErr(cb, err10) }
+                return ResOK(cb, { payload: ret10 })
+                break
+            case '100':
+                const [err100, ret100] = await new SysBillModel().calcMerchantStat(inparam)
+                if (err100) { return ResErr(cb, err100) }
+                return ResOK(cb, { payload: ret100 })
+                break
+            case '1000':
+                const [err1000, ret1000] = await new SysBillModel().calcAgentStat(inparam)
+                if (err1000) { return ResErr(cb, err1000) }
+                return ResOK(cb, { payload: ret1000 })
+                break
+            case '-1000':
+                const [err, ret] = await new SysBillModel().calcAgentAdminStat(inparam)
+                if (err) { return ResErr(cb, err) }
+                return ResOK(cb, { payload: ret })
+                break
+        }
+    } catch (error) {
+        console.error(error)
+        return ResErr(cb, error)
+    }
+}
+
+// ==================== 以下为内部方法 ====================
+export {
+    calcPlayerBill,                      //计算玩家账单
+    calcUserBill                         //计算用户账单
+}
---
+++ Stat/model/PlayerBillModel.js
@@ -0,0 +1,103 @@
+import { Tables, Store$, Codes, BizErr, Model, RoleCodeEnum, RoleModels } from '../lib/all'
+import { BaseModel } from './BaseModel'
+import _ from 'lodash'
+
+export class PlayerBillModel extends BaseModel {
+    constructor() {
+        super()
+        // 设置表名
+        this.params = {
+            TableName: Tables.HeraGamePlayerBill,
+        }
+        // 设置对象属性
+        this.item = {
+            ...this.baseitem
+        }
+    }
+    /**
+     * 计算用户流水
+     * @param {*} inparam 
+     */
+    async calcPlayerBill(inparam) {
+        let promiseArr = []
+        let self = this
+        for (let gameUserId of inparam.gameUserIds) {
+            let p = new Promise(function (resolve, reject) {
+                // 查询玩家账单并计算
+                let playerInparam = { gameUserId: gameUserId, gameType: inparam.gameType, createdAt: inparam.query.createdAt }
+                let playerPromise = self.calcPlayerFlow(playerInparam)
+                playerPromise.then((res) => {
+                    let bet = res.bet
+                    let winlose = res.winlose
+                    let betCount = res.betCount
+                    let winloseRate = bet == 0 ? 0 : +(winlose / bet).toFixed(2)
+                    console.log('玩家id:' + gameUserId + '玩家bet:' + bet + '玩家winlose:' + winlose + '次数:' + betCount + '比例:' + winloseRate)
+                    // 返回数据
+                    let finalResult = {
+                        gameUserId: gameUserId,
+                        bet: Math.abs(+bet.toFixed(2)),
+                        winlose: +winlose.toFixed(2),
+                        betCount: betCount,
+                        winloseRate: winloseRate,
+                    }
+                    console.log('玩家id:' + gameUserId + '执行成功:' + new Date().getTime())
+                    resolve(finalResult)
+                }).catch((err) => {
+                    console.error(err)
+                })
+            })
+            promiseArr.push(p)
+        }
+        // 并发所有玩家流水汇总查询
+        let finalRes = []
+        let start = new Date().getTime()
+        if (promiseArr.length > 0) {
+            finalRes = await Promise.all(promiseArr)
+        }
+        let end = new Date().getTime()
+        console.log('所有并发执行耗时：' + (end - start) + '毫秒')
+        console.log(finalRes)
+        return [false, finalRes]
+    }
+
+    /**
+     * 计算玩家流水
+     * @param {*} inparam 
+     */
+    calcPlayerFlow(inparam) {
+        let self = this
+        return new Promise(function (resolve, reject) {
+            console.log(inparam.gameUserId + '查询流水开始：' + new Date().getTime())
+            let query = {
+                IndexName: 'userIdIndex',
+                ProjectionExpression: 'amount,betAmount,reAmount',
+                KeyConditionExpression: '#userId  = :userId AND createAt between :createdAt0 AND :createdAt1',
+                FilterExpression: "gameType=:gameType",
+                ExpressionAttributeNames: {
+                    '#userId': 'userId',
+                },
+                ExpressionAttributeValues: {
+                    ':userId': parseInt(inparam.gameUserId),
+                    ':gameType': parseInt(inparam.gameType),
+                    ':createdAt0': parseInt(inparam.createdAt[0]),
+                    ':createdAt1': parseInt(inparam.createdAt[1]),
+                }
+            }
+            self.query(query).then((resArr) => {
+                let res = resArr[1]
+                console.log('查询结束：' + new Date().getTime() + '查询流水个数' + res.Items.length)
+                let bet = 0
+                let winlose = 0
+                let betCount = 0
+                for (let item of res.Items) {
+                    bet += parseFloat(item.betAmount)
+                    winlose += parseFloat(item.amount)
+                    betCount++
+                }
+                resolve({ bet: bet, winlose: winlose, betCount: betCount })
+            }).catch((err) => {
+                reject(err)
+            })
+        })
+    }
+}
