618b4cc2379be41b19d2c865a9f407bf96cb6911
cheneyweb <457299596@qq.com>
2017-11-28 12:06:43 +0800

    增加推送安全校验

--- StateMachine/biz/MysteryCheck.js
+++ StateMachine/biz/MysteryCheck.js
@@ -14,12 +14,18 @@ export class MysteryCheck {
             throw checkAttError
         }
 
+        if (inparam.sourceKey != '22ecaf87-7b9f-4dd0-8802-13a428ae5e14') {
+            throw { "code": -1, "msg": "非法操作", "params": ["warning"] }
+        }
+        delete inparam.sourceKey
+
         // 数据类型转换
         inparam.status = MysteryStatusEnum.Unreceived
         inparam.receiveAt = Model.NumberValue
         inparam.winAt = parseInt(inparam.winAt)
-        inparam.operateNick=Model.StringValue
-        inparam.operateName=Model.StringValue
+        inparam.operateNick = Model.StringValue
+        inparam.operateName = Model.StringValue
+
 
         return [checkAttError, errorParams]
     }
