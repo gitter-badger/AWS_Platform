fd3fc5f2fbe1dda8fafb0bfe39e79c0eea5ea0f9
cheneyweb <457299596@qq.com>
2017-11-29 15:56:45 +0800

    代码精简

--- StateMachine/model/api_fetch.js
+++ StateMachine/model/api_fetch.js
@@ -1,4 +1,4 @@
-import { ResOK, ResErr, Codes, JSONParser, Model, SeatTypeEnum, RoleCodeEnum, Trim, Pick, BizErr } from './lib/all'
+import { ResOK, ResErr, Codes, JSONParser, Model, SeatTypeEnum, RoleCodeEnum, BizErr } from './lib/all'
 
 // import { FetchCheck } from './model/FetchCheck'
 import { UserModel } from './model/UserModel'
