7b1fdcecad809fbf018b2b2892dcaf06234e9767
cheneyweb <457299596@qq.com>
2017-12-16 10:38:03 +0800

    升级自动构建

--- api-ttg/app.js
+++ api-ttg/app.js
@@ -1,7 +1,6 @@
 // 系统配置参数
 const config = require('config')
-// const { PORT = 80 } = process.env
-const PORT = 80
+const { PORT = 80 } = process.env
 // 应用服务与中间件相关
 const Koa = require('koa')
 // const koaBody = require('koa-body')
