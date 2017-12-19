5364a680c3d0949ddc8db9eb0c71a79ab97840ed
cheneyweb <457299596@qq.com>
2017-12-20 01:00:17 +0800

    服务合并

--- api-ttg/src/socket_game.js
+++ api-ttg/src/socket_game.js
@@ -6,21 +6,26 @@ const router = new Router()
 const PassThrough = require('stream').PassThrough
 
 router.get('/socket/balance', async function (ctx, next) {
+    // 生成数据
     const content = Date.now()
 
-    // ctx.req.on('close', ctx.res.end())
-    // ctx.req.on('finish', ctx.res.end())
-    // ctx.req.on('error', ctx.res.end())
-
+    // 实时数据
     ctx.type = 'text/event-stream'
     ctx.set('Cache-Control', 'no-cache')
     ctx.set('Connection', 'keep-alive')
     // ctx.set('Transfer-Encoding', 'chunked')
     const stream = new PassThrough()
-    setInterval(function () {
+    const timerId = setInterval(function () {
         console.info(`data:${content}\n\n`)
         stream.write(`data:${content}\n\n`)
     }, 1000)
+
+    // 错误关闭循环
+    ctx.req.on('close', clearInterval(timerId))
+    ctx.req.on('finish', clearInterval(timerId))
+    ctx.req.on('error', clearInterval(timerId))
+
+    // 数据推送
     ctx.body = stream
 })
 
